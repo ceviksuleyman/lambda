0334f95fbc8e51547257dc6a6194eb81294df3fc
cevik <suleymancevik64@gmail.com>
2022-08-20 16:02:14 +0300

    22-08-20

--- src/lambda_functional_programing/Fp04_MethodReference.java
+++ src/lambda_functional_programing/Fp04_MethodReference.java
@@ -25,6 +25,17 @@ public class Fp04_MethodReference {
         System.out.println();
         //elemanlariBuyukHarfIleYazdir02(liste);
         uzunlugaGoreYazdir(liste);
+        System.out.println();
+        uzunlugaGoreTerstenYazdir(liste);
+        System.out.println();
+        sonKaraktereGoreTekrarsizYazdir(liste);
+        System.out.println();
+        uzunlukVeIlkHarfeGoreSiralaYazdir(liste);
+        System.out.println();
+        //uzunlukBestenBuyukleriSil(liste);
+        //ilkVeSonKaraktereGoreSil01(liste);
+        //ilkVeSonKaraktereGoreSil02(liste);
+        uzunlukVeVerilenKaraktereGoreSil(liste);
 
     }
 
@@ -59,6 +70,94 @@ public class Fp04_MethodReference {
         //output : Ali Ali Mark Amanda Tucker Jackson Mariano Alberto Benjamin Christopher
 
     }//method son
+
+    public static void uzunlugaGoreTerstenYazdir(List<String> list) {
+
+        // 3 - Elemanlari uzunluklarina gore ters siralayip yazdiran bir method olusturun
+
+        list.stream().
+                sorted(Comparator.comparing(String::length).
+                        reversed()).
+                forEach(Utils::ayniSatirdaBosluklaYazdir);
+        //output:Christopher Benjamin Jackson Mariano Alberto Amanda Tucker Mark Ali Ali
+
+    }//method son
+
+    public static void sonKaraktereGoreTekrarsizYazdir(List<String> list) {
+
+        // 4 - Elemanlari son karakterlerine gore siralayip tekrarsiz yazdiran bir method olusturun
+
+        //sorted(Comparator.comparing(t -> t.charAt(t.length()-1)))
+        list.stream().distinct().
+                sorted(Comparator.comparing(Utils::sonKarakterAl)).
+                forEach(Utils::ayniSatirdaBosluklaYazdir);
+        //output:Amanda Ali Mark Jackson Benjamin Mariano Alberto Christopher Tucker
+
+    }//method son
+
+    public static void uzunlukVeIlkHarfeGoreSiralaYazdir(List<String> list) {
+
+        // 5 - Elemanlari once uzunluk ve sonra ilk karakterine gore siralayip yazdiran method olusturun
+
+        list.stream().sorted(Comparator.comparing(String::length).// uzunluk kontrol'den sonra
+                thenComparing(Utils::ilkKarakteriAl)). // sonra ilk harf kontrol
+                forEach(Utils::ayniSatirdaBosluklaYazdir);
+        //output:Ali Ali Mark Amanda Tucker Alberto Jackson Mariano Benjamin Christopher
+
+    }//method son
+
+
+    /*
+   public static void uzunlukBestenBuyukleriSil(List<String> list){
+
+       // 6 - Uzunlugu 5'ten buyuk olan elemanlari silen bir method olusturun
+
+       list.removeIf(t-> t.length() > 5); // uzunluk 5'ten buyuk ise sil
+
+       System.out.println(list); // [Ali, Ali, Mark]
+
+
+   }//method son
+   */
+
+
+
+     /*
+    public static void ilkVeSonKaraktereGoreSil01(List<String> list) { // 1.YONTEM
+
+        // 7 - ‘A’, ‘a’ ile baslayan ya da ‘N’, ‘n’ ile biten elemanlari silen bir method olusturun.
+
+        list.removeIf(t -> t.charAt(0) == 'a'
+                || t.charAt(0) == 'A'
+                || t.charAt(t.length() - 1) == 'n'
+                || t.charAt(t.length() - 1) == 'N');
+
+        System.out.println(list);// [Mark, Christopher, Mariano, Tucker]
+
+    }//method son
+     */
+
+    /*
+    public static void ilkVeSonKaraktereGoreSil02(List<String> list) { // 2.YONTEM
+
+        list.removeIf(t -> t.startsWith("A") || t.startsWith("a")
+                || t.endsWith("n") || t.endsWith("N"));
+
+        System.out.println(list);// [Mark, Christopher, Mariano, Tucker]
+
+    }//method son
+     */
+
+    public static void uzunlukVeVerilenKaraktereGoreSil(List<String> list) {
+
+        // 8 - uzunlugu 8 ile 10 arasinda olan ya da "o" ile biten elemanlari silen bir method olusturun
+
+        list.removeIf(t -> t.length() > 7 && t.length() < 11 || t.endsWith("o"));
+        // remove : Benjamin,Alberto,Mariano
+
+        System.out.println(list);// [Ali, Ali, Mark, Amanda, Christopher, Jackson, Tucker]
+
+    }// method son
 
 
 }
